dea16c8d8f93f20d7e81cbaed71bf23e810f4a46
MathieuDR <mathieu@deraedt.dev>
2026-02-15 03:40:15 +0100

    mechavolt;

---
+++ services/mechavolt.nix
@@ -0,0 +1,51 @@
+{
+  self,
+  config,
+  domainUtils,
+  ...
+}: let
+  listen_port = "9134";
+in {
+  age.secrets = {
+    "common/ghp".file = "${self}/secrets/common/ghp.age";
+  };
+
+  virtualisation.oci-containers.containers.mechavolt = {
+    image = "ghcr.io/MathieuDR/mechavolt:latest";
+    autoStart = true;
+
+    ports = [
+      "${listen_port}:80"
+    ];
+
+    # Auth
+    login = {
+      username = "MathieuDR";
+      registry = "https://ghcr.io";
+      passwordFile = config.age.secrets."common/ghp".path;
+    };
+  };
+
+  services.caddy.virtualHosts = {
+    ${domainUtils.domain "kenny"} = {
+      extraConfig = ''
+        reverse_proxy http://localhost:${listen_port}
+
+        encode {
+          zstd
+          gzip
+          minimum_length 1024
+        }
+
+        # Cache static assets
+        @static_files {
+          path /static/* /images/* /css/* /js/*
+        }
+
+        handle @static_files {
+          header Cache-Control "public, max-age=31536000, immutable"
+        }
+      '';
+    };
+  };
+}
